8acd7e493e1c0735f98437ad3b6b7eeef273daee
gmedairy <gmedairy@gmail.com>
2022-03-12 13:54:29 -0600

    First version

---
+++ src/MyFirstClass.java
@@ -0,0 +1,15 @@
+public class MyFirstClass {
+    public int a;
+
+    public int transform(int a) {
+        return a*3+4;
+    }
+
+    public static void main(String[] arg) {
+        MyFirstClass mfc = new MyFirstClass();
+        mfc.a = mfc.transform(7);
+        mfc.a = mfc.transform(mfc.a);
+        System.out.println("Transforming a " +mfc.a);
+
+    }
+}
